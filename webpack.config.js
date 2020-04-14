0627c8cd6d4bc4b8607c480fa2ac4191ff04cc09
Tommi Salonen <tommi.salonen@houston-inc.com>
2020-04-14 14:26:25 +0300

    Redirect 404 to index.html in dev-server
    To make routing work with direct urls in dev-environment.

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, './'), // where dev server will look for static files, not compiled
     publicPath: '/', //relative path to output path where  devserver will look for compiled files
+    historyApiFallback: true
   },
   output: {
     filename: 'js/[name].bundle.js',
