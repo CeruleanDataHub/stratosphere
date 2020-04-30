e4d23d03ef613e1b2a9c36fbecc81196947ca4c9
Muhammad Salman <salmanmanekia@gmail.com>
2020-04-30 12:17:42 +0300

    Adding/removing application acces from a user
    Co-authored-by: Miikka Alatalo <miikka.alatalo@houston-inc.com>
    Co-authored-by: Muhammad Salman <salmanmanekia@gmail.com>

--- src/containers/UserManagement/UserManagement.jsx
+++ src/containers/UserManagement/UserManagement.jsx
@@ -5,6 +5,8 @@ import Axios from 'axios';
 import {useRouteMatch, Link} from 'react-router-dom';
 import {Card} from '@denim/react-components';
 
+import './UserManagement.css';
+
 import env from '../../config';
 const UserManagementContainer = styled.section`
   margin-left: 18em;
@@ -23,6 +25,10 @@ const UserManagement = () => {
   let {url} = useRouteMatch();
 
   useEffect(() => {
+    getRolesAndPopulateList();
+  }, []);
+
+  const getRolesAndPopulateList = () => {
     Axios.get(`${baseDbApiUrl}/api/user-management/roles`)
       .then(async res => {
         if (res.data.status === 404) {
@@ -37,7 +43,7 @@ const UserManagement = () => {
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  };
 
   const setUsersWithRoles = (users, roles) => {
     if (!roles) return;
@@ -54,6 +60,8 @@ const UserManagement = () => {
     });
     Promise.all(responsePromises).then(() => {
       setUsers(users);
+      setInputEmail('');
+      setInputRoles('placeholder');
     });
   };
 
@@ -97,19 +105,25 @@ const UserManagement = () => {
 
   const handleSubmit = e => {
     const randomPw = 'abcdef' + Math.floor(Math.random() * 10000).toString();
+    const roles =
+      inputRoles === 'placeholder' || inputRoles === '' ? '' : [inputRoles];
     Axios.post(`${baseDbApiUrl}/api/user-management/users`, {
       email: inputEmail,
       password: randomPw,
-      roles: [inputRoles],
+      roles: roles,
     }).then(res => {
       console.log(res);
+      getRolesAndPopulateList();
     });
     e.preventDefault();
   };
 
   const renderRoles = roles => {
     return (
-      <select name="roles" onChange={onInputChange}>
+      <select name="roles" value={inputRoles} onChange={onInputChange}>
+        <option key="_placeholder" value="placeholder">
+          [select role]
+        </option>
         {roles.map(role => {
           return (
             <option key={role.id} value={role.id}>
@@ -124,7 +138,7 @@ const UserManagement = () => {
   return (
     <UserManagementContainer>
       <div style={{width: '50%'}}>
-        <Card item={{title: 'User Management'}}>
+        <Card height={'auto'} item={{title: 'User Management'}}>
           {users && users.length ? (
             <>
               <div className="list-columns">
@@ -159,16 +173,21 @@ const UserManagement = () => {
           )}
         </Card>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          id="username"
-          name="email"
-          onChange={onInputChange}
-        />
-        {Array.isArray(roles) && roles.length != 0 && renderRoles(roles)}
-        <button>Submit</button>
-      </form>
+      <div className="user-creation">
+        <div>New User Creation</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="email"
+            id="username"
+            name="email"
+            value={inputEmail}
+            onChange={onInputChange}
+          />
+          {Array.isArray(roles) && roles.length != 0 && renderRoles(roles)}
+          <button>Submit</button>
+        </form>
+      </div>
     </UserManagementContainer>
   );
 };
