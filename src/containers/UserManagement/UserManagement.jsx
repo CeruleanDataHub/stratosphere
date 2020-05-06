ddc972dfec4506ae890b1b05e724c8cfd0028caf
Muhammad Salman <salmanmanekia@gmail.com>
2020-05-06 16:07:20 +0300

    Moving the auth0 endpoint from db-api to proxy api
    Co-authored-by: Miikka Alatalo <miikka.alatalo@houston-inc.com>
    Co-authored-by: Muhammad Salman <salmanmanekia@gmail.com>

--- src/containers/UserManagement/UserManagement.jsx
+++ src/containers/UserManagement/UserManagement.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import {useEffect, useState} from 'react';
 import Axios from 'axios';
 import {useRouteMatch, Link} from 'react-router-dom';
+import {useAuth0} from '../../auth0-spa.jsx';
 import {Card} from '@denim/react-components';
 
 import './UserManagement.css';
@@ -14,28 +14,31 @@ const UserManagementContainer = styled.section`
 `;
 
 const envVar = env();
-const baseApiUrl = envVar.BASE_API_URL;
+const auth0ProxyUrl = envVar.AUTH0_PROXY_URL;
 
 const UserManagement = () => {
-  const [roles, setRoles] = useState([]);
   const [users, setUsers] = useState([]);
+  const {getTokenSilently} = useAuth0();
 
   const [inputEmail, setInputEmail] = useState('');
-  const [inputRoles, setInputRoles] = useState('');
   let {url} = useRouteMatch();
 
   useEffect(() => {
     getRolesAndPopulateList();
   }, []);
 
-  const getRolesAndPopulateList = () => {
-    Axios.get(`${baseApiUrl}/user-management/roles`)
+  const getRolesAndPopulateList = async () => {
+    const token = await getTokenSilently();
+    Axios.get(`${auth0ProxyUrl}/roles`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then(async res => {
         if (res.data.status === 404) {
           console.log('Error fetching roles');
         } else {
           const roles = res.data;
-          setRoles(roles);
           const users = await getUsers();
           setUsersWithRoles(users, roles);
         }
@@ -45,12 +48,15 @@ const UserManagement = () => {
       });
   };
 
-  const setUsersWithRoles = (users, roles) => {
+  const setUsersWithRoles = async (users, roles) => {
     if (!roles) return;
+    const token = await getTokenSilently();
     const responsePromises = roles.map(role => {
-      return Axios.get(
-        `${baseApiUrl}/user-management/roles/${role.id}/users`,
-      ).then(resp => {
+      return Axios.get(`${auth0ProxyUrl}/roles/${role.id}/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(resp => {
         if (resp.data.status === 404) {
           console.log('Error fetching users');
         } else {
@@ -61,12 +67,16 @@ const UserManagement = () => {
     Promise.all(responsePromises).then(() => {
       setUsers(users);
       setInputEmail('');
-      setInputRoles('placeholder');
     });
   };
 
-  const getUsers = () => {
-    return Axios.get(`${baseApiUrl}/user-management/users`)
+  const getUsers = async () => {
+    const token = await getTokenSilently();
+    return Axios.get(`${auth0ProxyUrl}/users`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then(res => {
         if (res.data.status === 404) {
           console.log('Error fetching users');
@@ -95,44 +105,33 @@ const UserManagement = () => {
   };
 
   const onInputChange = e => {
+    e.preventDefault();
     if (e.target.name === 'email') {
       setInputEmail(e.target.value);
-    } else {
-      setInputRoles(e.target.value);
     }
-    e.preventDefault();
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await getTokenSilently();
     const randomPw = 'abcdef' + Math.floor(Math.random() * 10000).toString();
-    const roles =
-      inputRoles === 'placeholder' || inputRoles === '' ? '' : [inputRoles];
-    Axios.post(`${baseApiUrl}/user-management/users`, {
-      email: inputEmail,
-      password: randomPw,
-      roles: roles,
-    }).then(res => {
+
+    Axios.post(
+      `${auth0ProxyUrl}/users`,
+      {
+        email: inputEmail,
+        password: randomPw,
+        connection: 'Username-Password-Authentication',
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    ).then(res => {
       console.log(res);
       getRolesAndPopulateList();
     });
-    e.preventDefault();
-  };
-
-  const renderRoles = roles => {
-    return (
-      <select name="roles" value={inputRoles} onChange={onInputChange}>
-        <option key="_placeholder" value="placeholder">
-          [select role]
-        </option>
-        {roles.map(role => {
-          return (
-            <option key={role.id} value={role.id}>
-              {role.name}
-            </option>
-          );
-        })}
-      </select>
-    );
   };
 
   return (
@@ -184,7 +183,6 @@ const UserManagement = () => {
             value={inputEmail}
             onChange={onInputChange}
           />
-          {Array.isArray(roles) && roles.length != 0 && renderRoles(roles)}
           <button>Submit</button>
         </form>
       </div>
