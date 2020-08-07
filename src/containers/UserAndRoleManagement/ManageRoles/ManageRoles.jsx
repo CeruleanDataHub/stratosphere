324cd8d901e13f51823c4c7d94d5f00b6236bb1b
Tommi <tommi.tarkki@gmail.com>
2020-08-07 14:13:41 +0300

    Start using modal for role management

--- src/containers/UserAndRoleManagement/ManageRoles/ManageRoles.jsx
+++ src/containers/UserAndRoleManagement/ManageRoles/ManageRoles.jsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {DataTable, Typography} from '@ceruleandatahub/react-components';
 import styled from 'styled-components';
-import ActionsCell from '../ActionsCell/ActionsCell.jsx';
-import getAllRoles from './getRoles/getRoles';
+
 import {useAuth0} from '../../../auth0-spa.jsx';
+import getAllRoles from './getRoles/getRoles';
+
+import ActionsCell from '../ActionsCell/ActionsCell.jsx';
 import SearchBar from '../SearchBar/SearchBar.jsx';
 import ManagementHeader from '../ManagementHeader/ManagementHeader.jsx';
+import RoleModal from './RoleModal/RoleModal.jsx';
 
 const ManageRolesContainer = styled.section`
   margin: 0 8em 2em 18em;
@@ -16,35 +19,39 @@ const actionsData = [
   {icon: 'chef-hat', text: 'Assign Permissions', modalToOpen: 'Permissions'},
 ];
 
-const setModalOpenTab = () => {};
-
-const cell = () => (
-  <ActionsCell setModalOpenTab={setModalOpenTab} actionsData={actionsData} />
-);
-
-const defaultRolesData = {
-  data: [],
-  columns: [
-    {id: 1, name: 'Name', selector: 'name'},
-    {id: 2, name: 'Users', selector: 'users'},
-    {
-      id: 3,
-      name: 'Permissions',
-      selector: 'permissions',
-    },
-    {
-      id: 4,
-      name: '',
-      selector: 'actions',
-      cell,
-    },
-  ],
-};
-
 const ManageRoles = () => {
   const {getTokenSilently} = useAuth0();
-  const [roleData, setRoleData] = useState(defaultRolesData);
+
+  const [roleModalOpenTab, setRoleModalOpenTab] = useState('');
   const [filterText, setFilterText] = useState('');
+
+  const cell = () => (
+    <ActionsCell
+      setModalOpenTab={setRoleModalOpenTab}
+      actionsData={actionsData}
+    />
+  );
+
+  const defaultRolesData = {
+    data: [],
+    columns: [
+      {id: 1, name: 'Name', selector: 'name'},
+      {id: 2, name: 'Users', selector: 'users'},
+      {
+        id: 3,
+        name: 'Permissions',
+        selector: 'permissions',
+      },
+      {
+        id: 4,
+        name: '',
+        selector: 'actions',
+        cell,
+      },
+    ],
+  };
+
+  const [roleData, setRoleData] = useState(defaultRolesData);
 
   useEffect(() => {
     const getRoles = async () => {
@@ -79,6 +86,12 @@ const ManageRoles = () => {
           data={roleData.data.filter(role =>
             role.name.toLowerCase().includes(filterText.toLowerCase()),
           )}
+        />
+
+        <RoleModal
+          isOpen={roleModalOpenTab !== ''}
+          roleModalOpenTab={roleModalOpenTab}
+          setRoleModalOpenTab={setRoleModalOpenTab}
         />
       </Typography>
     </ManageRolesContainer>
