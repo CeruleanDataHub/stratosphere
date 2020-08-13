fb326fe81afa690375b1756c052e534462bb3f0d
Tommi <tommi.tarkki@gmail.com>
2020-08-13 11:55:27 +0300

    Add support for custom margin and hideable search button

--- src/containers/UserAndRoleManagement/SearchBar/SearchBar.jsx
+++ src/containers/UserAndRoleManagement/SearchBar/SearchBar.jsx
@@ -17,42 +17,51 @@ const ButtonWithIcon = styled.button`
 `;
 
 const Form = styled.form`
-  margin: 50px 0;
+  margin: ${({margin}) => `${margin}px`} 0;
 `;
 
 const SearchButton = styled(ButtonWithIcon)`
   width: 100%;
 `;
-
 const submitHandler = event => {
   event.preventDefault();
 };
+const SearchBar = ({value, onChange, showSearchButton, margin}) => {
+  return (
+    <Form onSubmit={event => submitHandler(event)} margin={margin}>
+      <Grid columns={showSearchButton ? '4fr 1fr' : '5fr'}>
+        <Cell>
+          <Input
+            type="search"
+            placeholder="Search"
+            value={value}
+            onChange={onChange}
+          />
+        </Cell>
 
-const SearchBar = ({value, onChange}) => (
-  <Form onSubmit={event => submitHandler(event)}>
-    <Grid columns="4fr 1fr">
-      <Cell>
-        <Input
-          type="search"
-          placeholder="Search"
-          value={value}
-          onChange={onChange}
-        />
-      </Cell>
-
-      <Cell>
-        <Button type="submit" as={SearchButton}>
-          <Icon name="search" />
-          Search
-        </Button>
-      </Cell>
-    </Grid>
-  </Form>
-);
+        {showSearchButton && (
+          <Cell>
+            <Button type="submit" as={SearchButton}>
+              <Icon name="search" />
+              Search
+            </Button>
+          </Cell>
+        )}
+      </Grid>
+    </Form>
+  );
+};
 
 SearchBar.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  showSearchButton: PropTypes.bool,
+  margin: PropTypes.number,
+};
+
+SearchBar.defaultProps = {
+  showSearchButton: true,
+  margin: 50,
 };
 
 export default SearchBar;
